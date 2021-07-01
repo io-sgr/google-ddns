f83b4248bc9172c06bbb1d4741e38c9b3ccdcf93
SgrAlpha <admin@sgr.io>
2021-07-01 14:28:00 +0800

    Support IPv6

--- files/bin/google_ddns.sh
+++ files/bin/google_ddns.sh
@@ -15,17 +15,23 @@
 #!/usr/bin/env sh
 
 show_help() {
-   echo "Example: sh $0 --key-file=/credential.json \\"
-   echo "\t --zone=default --domain=example.com \\"
-   echo "\t --proxy-type=http --proxy-addr=localhost --proxy-port=8080"
-   echo ""
-   echo "Options:"
-   echo -e "\t -k|--key-file\tKey file"
-   echo -e "\t -z|--zone\tZone name"
-   echo -e "\t -d|--domain\tDomain name"
-   echo -e "\t -t|--proxy-type\tProxy type"
-   echo -e "\t -a|--proxy-addr\tProxy address"
-   echo -e "\t -p|--proxy-port\tProxy port"
+   cat << EOF
+
+Example:
+sh $0 \\
+   --key-file=/credential.json \\
+   --zone=default --domain=example.com \\
+   --proxy-type=http --proxy-addr=localhost --proxy-port=8080
+
+Options:
+   -k|--key-file	Key file
+   -z|--zone		Zone name
+   -d|--domain		Domain name
+   -t|--proxy-type	Proxy type
+   -a|--proxy-addr	Proxy address
+   -p|--proxy-port	Proxy port
+
+EOF
 }
 
 for i in "$@"
@@ -63,19 +69,25 @@ case $i in
 esac
 done
 
+if [[ -z "${KEY_FILE}" ]]; then
+    echo "Missing key file."
+    show_help
+    exit 1
+fi
+
 if [[ ! -f "${KEY_FILE}" ]]; then
-    echo "Key file not found! Location: ${KEY_FILE}"
+    echo "Key file not found at location: ${KEY_FILE}"
     exit 1
 fi
 
 if [[ -z "${ZONE}" ]]; then
-    echo "Missing ZONE."
+    echo "Missing zone."
     show_help
     exit 1
 fi
 
 if [[ -z "${DOMAIN_NAME}" ]]; then
-    echo "Missing DOMAIN."
+    echo "Missing domain."
     show_help
     exit 1
 fi
@@ -96,12 +108,6 @@ if [[ ! -z "${PROXY_PORT}" ]]; then
     gcloud config set proxy/port ${PROXY_PORT}
 fi
 
-gcloud dns record-sets list --project="${GCP_PROJECT}" --zone="${ZONE}" --filter="name=${DOMAIN_NAME}." --format=none
-if [[ 0 -ne "$?" ]]; then
-    echo "Unable to list record sets!"
-    exit 1
-fi
-
 submit_tx() {
     CHANGE_ID=`gcloud dns record-sets transaction execute --zone="${ZONE}" --format=json | jq .id --raw-output`
     if [[ -z "${CHANGE_ID}" ]]; then
@@ -116,22 +122,34 @@ submit_tx() {
     echo "Transaction is `gcloud dns record-sets changes describe ${CHANGE_ID} --zone="${ZONE}" --format=json | jq .status --raw-output`"
 }
 
-CURRENT_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-OLD_IP=`gcloud dns record-sets list --zone="${ZONE}" --filter="name=${DOMAIN_NAME}." --format=json | jq ".[0].rrdatas[0]" | jq "select(. != null)" --raw-output`
+update() {
+    TYPE=$1
 
-if [[ -z "${OLD_IP}" ]]; then
-    echo "No old IP address of '${DOMAIN_NAME}' found, creating type A record to "${CURRENT_IP}" ..."
-    gcloud dns record-sets transaction start --zone="${ZONE}"
-    gcloud dns record-sets transaction add --zone="${ZONE}" --name="${DOMAIN_NAME}." --type=A --ttl=300 "${CURRENT_IP}"
-    submit_tx
-elif [[ "${CURRENT_IP}" != "${OLD_IP}" ]]; then
-    echo "IP address of '${DOMAIN_NAME}' changed from '${OLD_IP}' to '${CURRENT_IP}', updating ..."
-    gcloud dns record-sets transaction start --zone="${ZONE}"
-    gcloud dns record-sets transaction remove --zone="${ZONE}" --name="${DOMAIN_NAME}." --type=A --ttl=300 "${OLD_IP}"
-    gcloud dns record-sets transaction add --zone="${ZONE}" --name="${DOMAIN_NAME}." --type=A --ttl=300 "${CURRENT_IP}"
-    submit_tx
-else
-    echo "IP address of '${DOMAIN_NAME}' is still '${OLD_IP}', no need to update."
-fi
+    CURRENT_IP=`dig +short myip.opendns.com "${TYPE}" @resolver1.opendns.com`
+    if [[ -z "${CURRENT_IP}" ]]; then
+        echo "No type '${TYPE}' IP address found, skipping ..."
+        return 0
+    fi
+
+    OLD_IP=`gcloud dns record-sets list --zone="${ZONE}" --name="${DOMAIN_NAME}." --type="${TYPE}" --format=json | jq ".[0].rrdatas[0]" | jq "select(. != null)" --raw-output`
+
+    if [[ -z "${OLD_IP}" ]]; then
+        echo "No old type '${TYPE}' IP address of '${DOMAIN_NAME}' found, creating type '${TYPE}' record to '${CURRENT_IP}' ..."
+        gcloud dns record-sets transaction start --zone="${ZONE}"
+        gcloud dns record-sets transaction add --zone="${ZONE}" --name="${DOMAIN_NAME}." --type="${TYPE}" --ttl=300 "${CURRENT_IP}"
+        submit_tx
+    elif [[ "${CURRENT_IP}" != "${OLD_IP}" ]]; then
+        echo "The type '${TYPE}' IP address of '${DOMAIN_NAME}' changed from '${OLD_IP}' to '${CURRENT_IP}', updating ..."
+        gcloud dns record-sets transaction start --zone="${ZONE}"
+        gcloud dns record-sets transaction remove --zone="${ZONE}" --name="${DOMAIN_NAME}." --type="${TYPE}" --ttl=300 "${OLD_IP}"
+        gcloud dns record-sets transaction add --zone="${ZONE}" --name="${DOMAIN_NAME}." --type="${TYPE}" --ttl=300 "${CURRENT_IP}"
+        submit_tx
+    else
+        echo "The type '${TYPE}' IP address of '${DOMAIN_NAME}' is still '${OLD_IP}', no need to update."
+    fi
+}
+
+update "AAAA"
+update "A"
 
 echo "Updated time: `date`"
